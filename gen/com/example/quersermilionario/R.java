049625603067d1b5364b44bd67b05d62ed516339
Betania <betacampello@hotmail.com>
2014-11-02 17:17:50 -0300

    coloquei o radio group

--- gen/com/example/quersermilionario/R.java
+++ gen/com/example/quersermilionario/R.java
@@ -1081,8 +1081,8 @@ containing a value of this type.
         public static final int ic_launcher=0x7f020058;
     }
     public static final class id {
-        public static final int Pause=0x7f06003e;
-        public static final int Spintypes=0x7f060041;
+        public static final int Pause=0x7f060044;
+        public static final int Spintypes=0x7f060047;
         public static final int action_bar=0x7f06001c;
         public static final int action_bar_activity_content=0x7f060015;
         public static final int action_bar_container=0x7f06001b;
@@ -1094,11 +1094,11 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f060016;
         public static final int action_menu_presenter=0x7f060017;
         public static final int action_mode_close_button=0x7f060024;
-        public static final int action_settings=0x7f060044;
+        public static final int action_settings=0x7f06004a;
         public static final int activity_chooser_view_content=0x7f060025;
         public static final int always=0x7f06000b;
         public static final int beginning=0x7f060011;
-        public static final int button1=0x7f060042;
+        public static final int button1=0x7f060048;
         public static final int checkbox=0x7f06002d;
         public static final int collapseActionView=0x7f06000d;
         public static final int default_activity_button=0x7f060028;
@@ -1117,17 +1117,22 @@ containing a value of this type.
         public static final int listMode=0x7f060001;
         public static final int list_item=0x7f060029;
         public static final int middle=0x7f060012;
-        public static final int name=0x7f060043;
+        public static final int name=0x7f060049;
         public static final int never=0x7f060009;
         public static final int none=0x7f060010;
         public static final int normal=0x7f060000;
-        public static final int play=0x7f060040;
+        public static final int play=0x7f060046;
         public static final int printName=0x7f06003c;
         public static final int printScore=0x7f06003d;
         public static final int progress_circular=0x7f060018;
         public static final int progress_horizontal=0x7f060019;
         public static final int radio=0x7f06002f;
-        public static final int ranking=0x7f06003f;
+        public static final int radio0=0x7f06003f;
+        public static final int radio1=0x7f060040;
+        public static final int radio2=0x7f060041;
+        public static final int radio3=0x7f060042;
+        public static final int radioGroup1=0x7f06003e;
+        public static final int ranking=0x7f060045;
         public static final int search_badge=0x7f060032;
         public static final int search_bar=0x7f060031;
         public static final int search_button=0x7f060033;
@@ -1145,6 +1150,7 @@ containing a value of this type.
         public static final int split_action_bar=0x7f06001e;
         public static final int submit_area=0x7f060039;
         public static final int tabMode=0x7f060002;
+        public static final int textView1=0x7f060043;
         public static final int title=0x7f06002b;
         public static final int top_action_bar=0x7f060020;
         public static final int up=0x7f060021;
